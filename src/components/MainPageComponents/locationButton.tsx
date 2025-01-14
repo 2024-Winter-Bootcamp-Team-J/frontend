377bf48e5632f8c525e6bd46b9c04de7e86e3270
yeonkyu LEE <yeonkyulee4@gmail.com>
2025-01-14 16:22:01 +0900

    [feat] 메인페이지 D3.js 차트 구현 완료 및 더미 데이터 적용 완료

--- src/components/MainPageComponents/locationButton.tsx
+++ src/components/MainPageComponents/locationButton.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import LocationIcon from '../../assets/LocationIcon.png';
+import React from 'react'
+import LocationIcon from '../../assets/LocationIcon.png'
 
-const LocationButton: React.FC = () => {
+type LocationButtonProps = {
+  moveToUserNode: () => void // 사용자 노드로 이동하는 함수
+}
+
+const LocationButton: React.FC<LocationButtonProps> = ({ moveToUserNode }) => {
   return (
-    <div className="flex items-center justify-center w-10 h-10 rounded-full shadow-md bg-customColor2/80 hover:bg-customColor2 background-blur-md">
+    <div
+      onClick={moveToUserNode} // 버튼 클릭 시 이동
+      className="flex items-center justify-center w-10 h-10 rounded-full shadow-md cursor-pointer bg-customColor2/80 hover:bg-customColor2 backdrop-blur-md"
+    >
       <img src={LocationIcon} alt="Location Icon" className="w-6 h-6" />
     </div>
-  );
-};
+  )
+}
 
-export default LocationButton;
+export default LocationButton
