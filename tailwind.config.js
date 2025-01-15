73af4a1c725a9f0ba34116fd18f096672350bdd4
yeonkyu LEE <yeonkyulee4@gmail.com>
2025-01-15 21:09:01 +0900

    [feat] 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,19 @@
 export default {
-
-  
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",'./src/styles/global.css' ],
-    theme: {
-      extend: {
-        blur:{
-          xs: '2px',
-        },
-        colors: {
-          customColor: '#282828', // 진한 희색
-          customColor2: '#444444', // 옅은 희색
-          recordColor: '#3A3A3A', // 더 옅은 희색
-          dateColor:'#737373', 
-          menuButton:'#6C6C6C',
-          primary: {
-            DEFAULT: "#2bca43",
-
-          },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/styles/global.css'],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#282828',
+        customColor2: '#444444',
+        recordColor: '#3A3A3A',
+        dateColor: '#737373',
+        menuButton: '#6C6C6C',
+        loading: '#464646', // 진한 회색
+        primary: {
+          DEFAULT: '#2bca43',
         },
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+}
