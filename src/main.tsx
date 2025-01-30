92ab3101b4031d50a7a316472c7ea12e52152316
yeonkyu LEE <yeonkyulee4@gmail.com>
2025-01-30 20:58:52 +0900

    [refactor] 자잘한 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import OnboardingPage from './page/onboarding'
-import MainPage from './page/mainPage'
-import './index.css' // import './styles/global.css'
-// import Nod from './components/Nods/Nod'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OnboardingPage />} />
-        <Route path="/main" element={<MainPage />} />
-
-        {/* <Route path="/nod" element={<Nod/>}/> */}
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
