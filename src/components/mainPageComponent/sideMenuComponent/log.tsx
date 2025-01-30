c209b13c6364dc660fc3f5730284430831509d69
이연규 <yeonkyulee4@gmail.com>
2025-01-31 01:52:54 +0900

    Revert "Refactor/#123 app main tsx refactor"

--- src/components/mainPageComponent/sideMenuComponent/log.tsx
+++ src/components/mainPageComponent/sideMenuComponent/log.tsx
@@ -7,27 +7,12 @@ type LogProps = {
 }
 
 const Log: React.FC<LogProps> = ({ content, name, createdAt }) => {
-  // createdAt을 Date 객체로 변환하고 포맷팅
-  const formatDate = (dateStr: string) => {
-    const date = new Date(dateStr)
-    const year = date.getFullYear().toString().slice(-2) // 마지막 두 자릿수만
-    const month = (date.getMonth() + 1).toString().padStart(2, '0') // 월은 0부터 시작하므로 +1
-    const day = date.getDate().toString().padStart(2, '0') // 두 자리로 맞추기
-    const hours = date.getHours().toString().padStart(2, '0') // 두 자리로 맞추기
-    const minutes = date.getMinutes().toString().padStart(2, '0') // 두 자리로 맞추기
-
-    return `${year}/${month}/${day} ${hours}:${minutes}`
-  }
-
-  const formattedDate = formatDate(createdAt)
-
-  console.log('Log Component Props:', { content, name, createdAt, formattedDate })
-
+  console.log('Log Component Props:', { content, name, createdAt })
   return (
     <div className="relative z-30 visible p-4 text-white border-r-2 bg-recordColor/60 border-y-2 border-customColor2 backdrop-blur-md">
       <div className="flex justify-between">
         <div className="text-[25px] text-white mb-3">{name}</div>
-        <div className="mt-3 text-dateColor">{formattedDate}</div>
+        <div className="mt-3 text-dateColor">{createdAt}</div>
       </div>
       <div className="relative overflow-hidden leading-relaxed text-justify text-white max-h-24">
         <p className="relative z-20">{content}</p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import OnboardingPage from './page/onboarding'
+import MainPage from './page/mainPage'
+import './index.css' // import './styles/global.css'
+// import Nod from './components/Nods/Nod'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<OnboardingPage />} />
+        <Route path="/main" element={<MainPage />} />
+
+        {/* <Route path="/nod" element={<Nod/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
